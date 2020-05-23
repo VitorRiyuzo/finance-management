960dd3940cba9f66f286bf41817895afd02e3ba3
VitorRiyuzo <vitor_riyuzo@hotmail.com>
2020-05-23 10:50:46 -0300

    Cadastro do usuário

--- src/main.js
+++ src/main.js
@@ -2,14 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router';
 import store from '@/store';
-
+import firebase from 'firebase';
 // import "spectre.css/dist/spectre.min.css";
 // import "spectre.css/dist/spectre-icons.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@/style.css";
+import "alertifyjs/build/css/alertify.min.css";
 Vue.config.productionTip = false
-
+var firebaseConfig = {
+  apiKey: "AIzaSyBrMz34tZom9w5zPzHjLAbxkUfrSDVh-NY",
+  authDomain: "finance-management-c5d76.firebaseapp.com",
+  databaseURL: "https://finance-management-c5d76.firebaseio.com",
+  projectId: "finance-management-c5d76",
+  storageBucket: "finance-management-c5d76.appspot.com",
+  messagingSenderId: "119785949890",
+  appId: "1:119785949890:web:eab4c7c9ce75b1d9f2895b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 new Vue({
   render: h => h(App),
   router,
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,18 +5,18 @@
                 <div class="jumbotron">
                     <h1 class="display-4">Cadastre-se</h1>
                     <hr class="my-4">
-                    <form>
+                    <form @submit.prevent="signup">
                         <div class="form-group">
                             <label>E-mai</label>
-                            <input type="email" class="form-control">
+                            <input type="email" v-model="email" class="form-control">
                         </div>
                         <div class="form-group">
                             <label>Senha</label>
-                            <input type="password" class="form-control">
+                            <input type="password" v-model="password" class="form-control">
                         </div>
                         <div class="form-group">
                             <label>Confirmar senha</label>
-                            <input type="password" class="form-control">
+                            <input type="password" v-model="passwordConfirm" class="form-control">
                         </div>
                         <div class="row">
                             <div class="col">
@@ -29,6 +29,50 @@
         </div>
     </div>
 </template>
-<style scoped>
-    
-</style>
+<style scoped></style>
+<script>
+    import Swal from 'sweetalert2';
+    import firebase from 'firebase';
+    export default {
+        data(){
+            
+            return{
+                email:'',
+                password:'',
+                passwordConfirm:''
+            }
+        },
+        methods:{
+            signup(){
+                if(this.password == this.passwordConfirm){
+                    firebase.auth().createUserWithEmailAndPassword(this.email,this.password).then((user)=>{
+                        console.log(user);
+                        Swal.fire({icon:"success", title:"Cadastro feito com sucesso!"});
+                        this.$router.push('/');
+                    },(error)=>{
+                        console.log(error);
+                        switch (error.code) {
+                            case "auth/invalid-email":
+                                error.message = "Email não é válido";
+                                break;
+                            case "auth/user-disable":
+                                error.message = "O usuário está desabilitado";
+                                break;
+                            case "auth/user-not-found":
+                                error.message = "Usuário não encontrado";
+                                break;
+                            case "auth/wrong-password":
+                                error.message = "Senha inválida";
+                                break;
+                            default: error.message = "Erro na autenticação, verifique sua conexão";
+                        }
+                        Swal.fire(error.message);
+                    })
+                }else{
+                    Swal.fire('Senhas não coinciden');
+                    this.$router.push('/')
+                }
+            }
+        }
+    }
+</script>
